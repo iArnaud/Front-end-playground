a6a41284d971e8097874aecb9682e585d71afc51
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-05-16 12:05:47 +0200

    Streaming it

--- mustache.js/main.js
+++ mustache.js/main.js
@@ -7,21 +7,33 @@
 
 "use strict";
 
+
 $(document).ready(function(){
-    Catastrophes.Flush();
-    setInterval(function(){Catastrophes.Flush()}, 10000);
+    Stream.Flush();
+    setInterval(function(){Stream.Flush()}, 10000);
 });
 
-var url = 'http://search.twitter.com/search.json?q=catastroph&rpp=50&include_entities=true&result_type=recent&lang=en';
+var search = 'web2day%20c%20est%20parti';
+var searchLabel = search.replace(/%20/gi, " ");
+console.log(searchLabel);
+var rpp = 100;
+
+var url = 'http://search.twitter.com/search.json?q='+search+'&rpp='+rpp+'&include_entities=true&result_type=recent';
+
+//url = "http://playground/mustache.js/search.json";
 
 var partials = {
-    header: '<h1><a href="' + url + '" target="_blank">Live catastrophes</a></h1><div id="content">'
+    header: '<h1><a id="titleLabel" href="' + url + '" target="_blank">Live<br>"' + searchLabel + '"</a></h1><div id="content">'
     ,footer: '</div>'
 };
 
-var Catastrophes = {
+var results;
+
+var Stream = {
     Flush: function(){
         $.getJSON(url, function(data){
+            results = data.results;
+            console.log(url, results);
             data.getDate = function() {
                 var dt = new Date(Date.parse(this.created_at));
                 var _date = dt.getDate() + '/'  + parseInt(dt.getMonth()+1) + '/' + dt.getFullYear();
@@ -68,6 +80,7 @@ var Catastrophes = {
             $('#tweets').fadeOut("fast", function(){
                 $('#tweets').fadeIn("fast", function(){
                     $('#tweets').html(Mustache.to_html($('#template').html(), data, partials));
+                    $('a#titleLabel').html($('a#titleLabel').html() + "<br>(" + data.results.length + " found)");
                 });
             });
         });
